9524fe5dbc9767adc44b8d8706542050ed8eb277
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2024-03-27 21:49:06 -0300

    🎨 Style: update

--- src/components/secao2.tsx
+++ src/components/secao2.tsx
@@ -4,7 +4,7 @@ import CarouselSecao2 from "./carousel-secao2";
 
 export default function Secao2() {
   return (
-    <div className="bg-bg2  justify-center  bg-right-top bg-no-repeat pt-12 md:bg-top md:pb-14">
+    <div className="justify-center bg-bg2 bg-right-top bg-no-repeat pt-12 md:bg-top md:pb-14">
       <div className="container w-full max-w-7xl">
         <p className="text-center text-lg font-bold md:text-3xl lg:mt-12 lg:text-4xl">
           O QUE VOCE IRA APRENDER
