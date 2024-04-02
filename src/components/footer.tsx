091c8610521330c1defb8184520161e788b5b401
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2024-04-02 19:56:03 -0300

    💬 Texts: putting a link to my portfolio in the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="bg-primary py-4">
-      <div className="container flex w-full max-w-7xl flex-col items-center justify-center gap-1 text-xs text-white md:flex-row  md:text-base">
+      <div className="container flex w-full max-w-7xl flex-col items-center justify-center gap-1 text-xs text-white md:text-base  lg:flex-row">
         <span>TODOS OS DIREITOS RESERVADOS</span>
         <span>POLITICA DE PRIVACIDADE — TERMOS DE USO</span>
+        <span className="hidden lg:block"> — </span>
+        <Link href={"felipe-lacerda.vercel.app"} className="hover:text-black">
+          FEITO POR FELIPE LACERDA
+        </Link>
       </div>
     </footer>
   );
